2443232b73af2f90545e8cf3ce8662c4ae7c21f8
Hugo Pascual Adán <hugopascual998@gmail.com>
2022-11-29 17:17:16 +0100

    some last tests

--- docker/restart.sh
+++ docker/restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker-compose down && sudo docker volume prune -f
-sudo docker-compose up -d
+sudo docker compose down --remove-orphans && sudo docker volume prune -f
+sudo docker compose up -d
--- docker/stop.sh
+++ docker/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker-compose down && sudo docker volume prune -f
+sudo docker compose down --remove-orphans && sudo docker volume prune -f
